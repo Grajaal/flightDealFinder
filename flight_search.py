9d5e07db1de212844dd06b9e5d3a321f3cc7e383
Grajaal <jorgegrajal@gmail.com>
2023-10-28 21:32:14 +0200

    Checking that the sheety API is working

--- flight_search.py
+++ flight_search.py
@@ -1,3 +1,22 @@
+import requests
+
+SHEET_ENDPOINT = "https://api.sheety.co/284278c5d757ef8dad6843e2345be840/flightDeals/prices"
+DATA = {
+    "price": {
+        "iataCode": "TESTING"
+    }
+}
+
+
 class FlightSearch:
-    #This class is responsible for talking to the Flight Search API.
-    pass
+    # This class is responsible for talking to the Flight Search API.
+    def __init__(self, cities):
+        self.cities = cities
+
+    def do(self):
+        for city in self.cities:
+            print(city[1])
+            row_endpoint = f"{SHEET_ENDPOINT}/{city[1]}"  # (city, id) -> [1] is the id
+            response = requests.put(url=row_endpoint, json=DATA)
+
+            print(response.json())
--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 import requests
+from pprint import pprint
+from flight_search import FlightSearch
 
-SHEETY_ENDPOINT = "https://api.sheety.co/284278c5d757ef8dad6843e2345be840/flightDeals/prices"
+SHEET_ENDPOINT = "https://api.sheety.co/284278c5d757ef8dad6843e2345be840/flightDeals/prices"
 
+sheet_data = requests.get(SHEET_ENDPOINT).json()
+
+cities = [(city['city'], city['id']) for city in sheet_data['prices']]
+
+fs = FlightSearch(cities)
+
+fs.do()
